4df71fd16a6c79aef918fa8607f7e2d1e16be259
Kesavaraj80 <mkesavaraj1998@gmail.com>
2021-12-11 16:06:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
+import Carousel from 'react-material-ui-carousel'
 import './App.css';
 
 function App() {
+
+  const receipies = [{
+    imageurl: "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/gnocchi-1d16725.jpg?quality=90&webp=true&resize=375,341",
+    name: "Chorizo & mozzarella gnocchi bake"
+  },
+  {
+    imageurl: "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/butter-chicken-cf6f9e2.jpg?quality=90&webp=true&resize=375,341",
+    name: "Easy butter chicken"
+  },
+  {
+    imageurl: "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/classic-lasange-4a66137.jpg?quality=90&webp=true&resize=375,341",
+    name: "Easy classic lasagne"
+  },
+  {
+    imageurl: "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/easy-teriyaki-c845724.jpg?quality=90&webp=true&resize=375,341",
+    name: "Easy teriyaki chicken"
+  }];
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Carousel className="App"
+    autoPlay={true}
+    animation="slide"
+    >
+      {
+        receipies.map((item)=>{
+          return(
+            <div className="receipie_container">
+              <img src={item.imageurl}alt={item.name} className="r_image"></img>
+              <h1>{item.name}</h1>
+            </div>
+          )
+        })
+      }
+    </Carousel>
   );
 }
 
